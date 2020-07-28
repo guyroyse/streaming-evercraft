7db3cfc8c8c5ffca88d4d0561608dfd7b8940384
Guy Royse <guy@guyroyse.com>
2020-07-27 20:08:57 -0400

    more hitpoint as you level

--- spec/hero-spec.js
+++ spec/hero-spec.js
@@ -54,23 +54,20 @@ describe("Hero", () => {
   })
 
   describe("#hitPoints", () => {
-    it("defaults to 5", () => {
-      expect(subject.hitPoints).toBe(5)
-    })
 
-    it("goes up when hero is buff", () => {
-      subject.constitution.score = 14
-      expect(subject.hitPoints).toBe(7)
-    })
-
-    it("goes down when hero is sickly", () => {
-      subject.constitution.score = 6
-      expect(subject.hitPoints).toBe(3)
-    })
-
-    it("cannot goes below 0 because of constitution", () => {
-      subject.constitution.score = 1
-      expect(subject.hitPoints).toBe(1)
+    it.each([
+      ["defaults to 5", { xp: 0, con: 10, hp: 5 }],
+      ["goes up when hero is buff", { xp: 0, con: 14, hp: 7 }],
+      ["goes down when hero is sickly", { xp: 0, con: 6, hp: 3 }],
+      ["cannot go below 0 because of constitution", { xp: 0, con: 1, hp: 1 }],
+      ["goes up with levels", { xp: 2000, con: 10, hp: 15 }],
+      ["goes up with levels and buffitude", { xp: 2000, con: 14, hp: 21 }],
+      ["still goes up level even if sickly", { xp: 2000, con: 6, hp: 9 }],
+      ["has at least 1 hp per level", { xp: 2000, con: 1, hp: 3 }]
+    ])("%s", (_, data) => {
+      subject.addExperience(data.xp)
+      subject.constitution.score = data.con
+      expect(subject.hitPoints).toBe(data.hp)
     })
 
     it("doesn't goes down when damaged", () => {
@@ -176,4 +173,18 @@ describe("Hero", () => {
     })
   })
 
+  describe("#level", () => {
+    it.each([
+      [{ xp: 0, level: 1 }],
+      [{ xp: 500, level: 1 }],
+      [{ xp: 999, level: 1 }],
+      [{ xp: 1000, level: 2 }],
+      [{ xp: 1500, level: 2 }],
+      [{ xp: 2000, level: 3 }],
+      [{ xp: 5000, level: 6 }]
+    ])("has expected value", (data) => {
+      subject.addExperience(data.xp)
+      expect(subject.level).toBe(data.level)
+    })  
+  })
 })
