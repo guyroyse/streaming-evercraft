f654d29ab946e7c5fc4d736792abb474b621578a
Guy Royse <guy@guyroyse.com>
2020-08-17 20:37:10 -0400

    added ac for monk

--- spec/hero-armor-class-spec.js
+++ spec/hero-armor-class-spec.js
@@ -20,5 +20,45 @@ describe("Hero", () => {
       subject.dexterity.score = 6
       expect(subject.armorClass).toBe(8)
     })
+
+    describe("when hero is a monk", () => {
+      beforeEach(() => subject.class = 'Monk') 
+
+      describe("and is very wise", () => {
+        beforeEach(() => subject.wisdom.score = 14) 
+
+        it("add wisdom modifier to armor class", () => {
+          expect(subject.armorClass).toBe(12)
+        })
+
+        it("include dexterity modifier when hero is zippy", () => {
+          subject.dexterity.score = 14
+          expect(subject.armorClass).toBe(14)
+        })
+    
+        it("include dexterity modifier when hero is sluggish", () => {
+          subject.dexterity.score = 6
+          expect(subject.armorClass).toBe(10)
+        })
+      })
+
+      describe("and is a bit foolish", () => {
+        beforeEach(() => subject.wisdom.score = 6)
+
+        it("add does not apply wisdom penalty to armor class", () => {
+          expect(subject.armorClass).toBe(10)
+        })
+
+        it("include dexterity modifier when hero is zippy", () => {
+          subject.dexterity.score = 14
+          expect(subject.armorClass).toBe(12)
+        })
+    
+        it("include dexterity modifier when hero is sluggish", () => {
+          subject.dexterity.score = 6
+          expect(subject.armorClass).toBe(8)
+        })
+      })
+    })
   })
 })
