7b083963d840460010e88996609643afa354783d
Guy Royse <guy@guyroyse.com>
2020-09-14 21:04:38 -0400

    paladin is complete

--- spec/hero-attack-damage-spec.js
+++ spec/hero-attack-damage-spec.js
@@ -16,9 +16,9 @@ describe("Hero", () => {
       let DEFAULTS = { class: clazz, str: 10, opponentAlignment: 'Neutral' }
 
       it.each([
-        ["defaults to 1",                        { ...DEFAULTS, attackDamage: 1 }],
-        ["goes up when hero is beefy",           { ...DEFAULTS, str: 14, attackDamage: 3 }],
-        ["is at least 1 regardless of strength", { ...DEFAULTS, str: 6, attackDamage: 1 }],
+        ["defaults to 1",                        { ...DEFAULTS, damage: 1 }],
+        ["goes up when hero is beefy",           { ...DEFAULTS, str: 14, damage: 3 }],
+        ["is at least 1 regardless of strength", { ...DEFAULTS, str: 6, damage: 1 }],
       ])("%s", (_, data) => validateAttackDamage(data))
     })
 
@@ -27,10 +27,10 @@ describe("Hero", () => {
       let DEFAULTS = { class: 'Monk', str: 10 , opponentAlignment: 'Neutral' }
 
       it.each([
-        ["defaults to 3",                        { ...DEFAULTS, attackDamage: 3 }],
-        ["goes up when monk is beefy",           { ...DEFAULTS, str: 14, attackDamage: 5 }],
-        ["goes down when monk is wimpy",         { ...DEFAULTS, str: 6, attackDamage: 1 }],
-        ["is at least 1 regardless of strength", { ...DEFAULTS, str: 1, attackDamage: 1 }],
+        ["defaults to 3",                        { ...DEFAULTS, damage: 3 }],
+        ["goes up when monk is beefy",           { ...DEFAULTS, str: 14, damage: 5 }],
+        ["goes down when monk is wimpy",         { ...DEFAULTS, str: 6, damage: 1 }],
+        ["is at least 1 regardless of strength", { ...DEFAULTS, str: 1, damage: 1 }],
       ])("%s", (_, data) => validateAttackDamage(data))
     })
 
@@ -39,12 +39,12 @@ describe("Hero", () => {
       let DEFAULTS = { class: 'Paladin', str: 10, opponentAlignment: 'Neutral' }
 
       it.each([
-        ["defaults to 1",                            { ...DEFAULTS, attackDamage: 1 }],
-        ["goes up when paladin is beefy",            { ...DEFAULTS, str: 14, attackDamage: 3 }],
-        ["goes up by +2 when opponent is evil",      { ...DEFAULTS, opponentAlignment: 'Evil', attackDamage: 3}],
-        ["does not go up when opponent is good",     { ...DEFAULTS, opponentAlignment: 'Good', attackDamage: 1}],
-        ["goes down with paladin is wimpy vs. evil", { ...DEFAULTS, str: 6, opponentAlignment: 'Evil', attackDamage: 1}],
-        ["is at least 1 regardless of strength",     { ...DEFAULTS, str: 1, opponentAlignment: 'Evil', attackDamage: 1}],
+        ["defaults to 1",                            { ...DEFAULTS, damage: 1 }],
+        ["goes up when paladin is beefy",            { ...DEFAULTS, str: 14, damage: 3 }],
+        ["goes up by +2 when opponent is evil",      { ...DEFAULTS, opponentAlignment: 'Evil', damage: 3}],
+        ["does not go up when opponent is good",     { ...DEFAULTS, opponentAlignment: 'Good', damage: 1}],
+        ["goes down with paladin is wimpy vs. evil", { ...DEFAULTS, str: 6, opponentAlignment: 'Evil', damage: 1}],
+        ["is at least 1 regardless of strength",     { ...DEFAULTS, str: 1, damage: 1}],
       ])("%s", (_, data) => validateAttackDamage(data))
     })
 
@@ -55,60 +55,68 @@ describe("Hero", () => {
       makeClass(subject, data.class)
 
       subject.strength.score = data.str
-      expect(subject.attackDamage(defender)).toBe(data.attackDamage)
+      expect(subject.attackDamage(defender)).toBe(data.damage)
     }
   })
 
-
   describe("#criticalDamage", () => {
-    it("default to 2", () => {
-      expect(subject.criticalDamage(defender)).toBe(2)
+
+    describe.each(['None', 'Fighter'])("for %s", (clazz) => {
+
+      let DEFAULTS = { class: clazz, str: 10, opponentAlignment: 'Neutral' }
+
+      it.each([
+        ["defaults to 2",                        { ...DEFAULTS, damage: 2 }],
+        ["goes up when hero is beefy",           { ...DEFAULTS, str: 14, damage: 6 }],
+        ["is at least 1 regardless of strength", { ...DEFAULTS, str: 6, damage: 1 }],
+      ])("%s", (_, data) => validateCriticalDamage(data))
     })
 
-    it("goes up when hero is beefy", () => {
-      subject.strength.score = 14
-      expect(subject.criticalDamage(defender)).toBe(6)
+    describe("for Rogue", () => {
+      
+      let DEFAULTS = { class: 'Rogue', str: 10 , opponentAlignment: 'Neutral' }
+
+      it.each([
+        ["defaults to 3",                        { ...DEFAULTS, damage: 3 }],
+        ["goes up when rogue is beefy",          { ...DEFAULTS, str: 14, damage: 9 }],
+        ["is at least 1 regardless of strength", { ...DEFAULTS, str: 1, damage: 1 }],
+      ])("%s", (_, data) => validateCriticalDamage(data))
     })
 
-    it("cannot go below 1", () => {
-      subject.strength.score = 6
-      expect(subject.criticalDamage(defender)).toBe(1)
+    describe("for Monk", () => {
+      
+      let DEFAULTS = { class: 'Monk', str: 10 , opponentAlignment: 'Neutral' }
+
+      it.each([
+        ["defaults to 6",                        { ...DEFAULTS, damage: 6 }],
+        ["goes up when monk is beefy",           { ...DEFAULTS, str: 14, damage: 10 }],
+        ["goes down when monk is wimpy",         { ...DEFAULTS, str: 6, damage: 2 }],
+        ["is at least 1 regardless of strength", { ...DEFAULTS, str: 1, damage: 1 }],
+      ])("%s", (_, data) => validateCriticalDamage(data))
     })
 
-    describe("when a rogue", () => {
-      beforeEach(() => subject.class = 'Rogue')
+    describe("for Paladin", () => {
 
-      it("default to 3", () => {
-        expect(subject.criticalDamage(defender)).toBe(3)
-      })
-  
-      it("goes up when hero is beefy", () => {
-        subject.strength.score = 14
-        expect(subject.criticalDamage(defender)).toBe(9)
-      })
-  
-      it("cannot go below 1", () => {
-        subject.strength.score = 6
-        expect(subject.criticalDamage(defender)).toBe(1)
-      })  
+      let DEFAULTS = { class: 'Paladin', str: 10, opponentAlignment: 'Neutral' }
+
+      it.each([
+        ["defaults to 2",                              { ...DEFAULTS, damage: 2 }],
+        ["goes up when paladin is beefy",              { ...DEFAULTS, str: 14, damage: 6 }],
+        ["goes up by +2 and x3 when opponent is evil", { ...DEFAULTS, opponentAlignment: 'Evil', damage: 9}],
+        ["does not go up when opponent is good",       { ...DEFAULTS, opponentAlignment: 'Good', damage: 2}],
+        ["goes down when paladin is wimpy",            { ...DEFAULTS, str: 6, opponentAlignment: 'Evil', damage: 3}],
+        ["is at least 1 regardless of strength",       { ...DEFAULTS, str: 1, damage: 1}],
+      ])("%s", (_, data) => validateCriticalDamage(data))
     })
 
-    describe("when a monk", () => {
-      beforeEach(() => subject.class = 'Monk')
+    function validateCriticalDamage(data) {
+      let defender = new Hero()
+      defender.alignment = data.opponentAlignment
 
-      it("default to 6", () => {
-        expect(subject.criticalDamage(defender)).toBe(6)
-      })
-  
-      it("goes up when hero is beefy", () => {
-        subject.strength.score = 14
-        expect(subject.criticalDamage(defender)).toBe(10)
-      })
-  
-      it("cannot go below 1", () => {
-        subject.strength.score = 1
-        expect(subject.criticalDamage(defender)).toBe(1)
-      })  
-    })
+      makeClass(subject, data.class)
+
+      subject.strength.score = data.str
+      expect(subject.criticalDamage(defender)).toBe(data.damage)
+    }
   })
 })
