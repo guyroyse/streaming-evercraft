be7b4fd292581a74ffa8824cb6d4ce0bb6e4e304
Guy Royse <guy@guyroyse.com>
2020-10-05 20:11:27 -0400

    armor class test and code hit all permutaitons for orc, monk, dex, etc

--- spec/hero-armor-class-spec.js
+++ spec/hero-armor-class-spec.js
@@ -8,59 +8,75 @@ describe("Hero", () => {
 
   describe("#armorClass", () => {
 
-    let DEFAULTS = { attackerClass: 'None', defenderRace: 'Human', defenderClass: 'None', dex: 10, wis: 10, ac: 10 }
+    let DEFAULTS = { attackerClass: 'None', defenderRace: 'Human', defenderClass: 'None', dex: 10, wis: 10 }
 
-    describe("typical scenario", () => {
+    describe("for normies", () => {
       it.each( [
-        ["defaults to 10",                                     { ...DEFAULTS }],
-        ["include dexterity modifier when hero is zippy",      { ...DEFAULTS, dex: 14, ac: 12 }],
-        ["include dexterity modifier when hero is sluggish",   { ...DEFAULTS, dex: 6, ac: 8 }],
-        ["when defender is an orc, +2 is added",               { ...DEFAULTS, defenderRace: 'Orc', ac: 12 }],
-        ["when defender is a zippy orc, +2 is still added",    { ...DEFAULTS, defenderRace: 'Orc', dex: 14, ac: 14 }],
-        ["when defender is a sluggish orc, +2 is still added", { ...DEFAULTS, defenderRace: 'Orc', dex: 6, ac: 10 }],
+        ["defaults to 10",                                                     { ...DEFAULTS, ac: 10 }],
+        ["includes DEX when hero is zippy",                                    { ...DEFAULTS, dex: 14, ac: 12 }],
+        ["includes DEX when hero is sluggish",                                 { ...DEFAULTS, dex: 6, ac: 8 }],
+        ["doesn't include DEX when attacker is a rogue and the hero is zippy", { ...DEFAULTS, attackerClass: 'Rogue', dex: 14, ac: 10 }],
+        ["includes DEX when attacker is a rogue and the hero is sluggish",     { ...DEFAULTS, attackerClass: 'Rogue', dex: 6, ac: 8 }],
+        ])("%s", (_, data) => validateArmorClass(data))
+    })
+
+    describe("for orcs", () => {
+      let ORC = { ...DEFAULTS, defenderRace: 'Orc' }
+      it.each( [
+        ["adds +2 to default",                         { ...ORC, ac: 12 }],
+        ["includes DEX when zippy",                    { ...ORC, dex: 14, ac: 14 }],
+        ["includes DEX when sluggish",                 { ...ORC, dex: 6, ac: 10 }],
+        ["includes +2 bonus vs. a rogue",              { ...ORC, attackerClass: 'Rogue', ac: 12 }],
+        ["doesn't include DEX when zippy vs. a rogue", { ...ORC, attackerClass: 'Rogue', dex: 14, ac: 12 }],
+        ["includes DEX when sluggish vs. a rogue",     { ...ORC, attackerClass: 'Rogue', dex: 6, ac: 10 }],
       ])("%s", (_, data) => validateArmorClass(data))
     })
 
-    it.each([
-      ["when hero is a wise old monk",                      { ...DEFAULTS, defenderClass: 'Monk', wis: 14, ac: 12 }],
-      ["when hero is a wise and zippy monk",                { ...DEFAULTS, defenderClass: 'Monk', dex: 14, wis: 14, ac: 14 }],
-      ["when hero is a wise and sluggish monk",             { ...DEFAULTS, defenderClass: 'Monk', dex: 6, wis: 14, ac: 10 }],
-      ["when hero is a foolish old monk",                   { ...DEFAULTS, defenderClass: 'Monk', wis: 6, ac: 10 }],
-      ["when hero is a foolish and zippy monk",             { ...DEFAULTS, defenderClass: 'Monk', dex: 14, wis: 6, ac: 12 }],
-      ["when hero is a foolish and sluggish monk",          { ...DEFAULTS, defenderClass: 'Monk', dex: 6, wis: 6, ac: 8 }],
-      ["when attacker is a rogue and the hero is zippy",    { ...DEFAULTS, attackerClass: 'Rogue', dex: 14, ac: 10 }],
+    describe("for monks", () => {
+      let MONK = { ...DEFAULTS, defenderClass: 'Monk' }
+      it.each( [
+        ["defaults to 10",                                                 { ...MONK, ac: 10 }],
+        ["includes DEX when zippy",                                        { ...MONK, dex: 14, ac: 12 }],
+        ["includes DEX when sluggish",                                     { ...MONK, dex: 6, ac: 8 }],
+        ["includes WIS when wise",                                         { ...MONK, wis: 14, ac: 12 }],
+        ["doesn't include WIS foolish",                                    { ...MONK, wis: 6, ac: 10 }],
+        ["includes DEX and WIS when zippy and wise",                       { ...MONK, dex: 14, wis: 14, ac: 14 }],
+        ["includes DEX only when zippy and foolish",                       { ...MONK, dex: 14, wis: 6, ac: 12 }],
+        ["includes DEX and WIS when sluggish and wise",                    { ...MONK, dex: 6, wis: 14, ac: 10 }],
+        ["includes DEX only when sluggish and foolish",                    { ...MONK, dex: 6, wis: 6, ac: 8 }],
+        ["doesn't include DEX when zippy vs. a rogue",                     { ...MONK, attackerClass: 'Rogue', dex: 14, ac: 10 }],
+        ["includes DEX when sluggish vs. a rogue",                         { ...MONK, attackerClass: 'Rogue', dex: 6, ac: 8 }],
+        ["doesn't include WIS when wise vs. a rogue",                      { ...MONK, attackerClass: 'Rogue', wis: 14, ac: 10 }],
+        ["doesn't include WIS when foolish vs. a rogue",                   { ...MONK, attackerClass: 'Rogue', wis: 6, ac: 10 }],
+        ["doesn't include DEX nor WIS when zippy and wise vs. a rogue",    { ...MONK, attackerClass: 'Rogue', dex: 14, wis: 14, ac: 10 }],
+        ["doesn't include DEX when zippy and foolish vs. a rogue",         { ...MONK, attackerClass: 'Rogue', dex: 14, wis: 6, ac: 10 }],
+        ["doesn't include DEX and WIS when sluggish and wise vs. a rogue", { ...MONK, attackerClass: 'Rogue', dex: 6, wis: 14, ac: 8 }],
+        ["includes DEX when sluggish and foolish vs. a rogue",             { ...MONK, attackerClass: 'Rogue', dex: 6, wis: 6, ac: 8 }],
+      ])("%s", (_, data) => validateArmorClass(data))
+    })
 
-      ["when attacker is a rogue and the hero is sluggish", 
-        { ...DEFAULTS, attackerClass: 'Rogue', dex: 6, ac: 8 }],
-
-      ["when attacker is a rogue and the hero is a wise old monk",
-        { ...DEFAULTS, attackerClass: 'Rogue', defenderClass: 'Monk', wis: 14, ac: 10 }],
-
-      ["when attacker is a rogue and the hero is a wise and zippy monk",
-        { ...DEFAULTS, attackerClass: 'Rogue', defenderClass: 'Monk', dex: 14, wis: 14, ac: 10 }],
-
-      ["when attacker is a rogue and the hero is a wise and sluggish monk",
-        { ...DEFAULTS, attackerClass: 'Rogue', defenderClass: 'Monk', dex: 6, wis: 14, ac: 8 }],
-
-      ["when attacker is a rogue and the hero is a foolish old monk",
-        { ...DEFAULTS, attackerClass: 'Rogue', defenderClass: 'Monk', wis: 6, ac: 10 }],
-
-      ["when attacker is a rogue and the hero is a foolish and zippy monk",
-        { ...DEFAULTS, attackerClass: 'Rogue', defenderClass: 'Monk', dex: 14, wis: 6, ac: 10 }],
-
-      ["when attacker is a rogue and the hero is a foolish and sluggish monk",
-        { ...DEFAULTS, attackerClass: 'Rogue', defenderClass: 'Monk', dex: 6, wis: 6, ac: 8 }],
-
-      ["when attacker is a rogue and the defender is an orc, +2 is still added",
-        { ...DEFAULTS, attackerClass: 'Rogue', defenderRace: 'Orc', ac: 12 }],
-
-      ["when attacker is a rogue and the defender is a zippy orc, +2 is still added",
-        { ...DEFAULTS, attackerClass: 'Rogue', defenderRace: 'Orc', dex: 14, ac: 12 }],
-
-      ["when attacker is a rogue and the defender is a sluggish orc, +2 is still added",
-        { ...DEFAULTS, attackerClass: 'Rogue', defenderRace: 'Orc', dex: 6, ac: 10 }],
-
-    ])("%s", (_, data) => validateArmorClass(data))
+    describe("for orcish monks", () => {
+      let ORCISH_MONK = { ...DEFAULTS, defenderRace: 'Orc', defenderClass: 'Monk' }
+      it.each( [
+        ["adds +2 to default",                                               { ...ORCISH_MONK, ac: 12 }],
+        ["include DEX when zippy",                                           { ...ORCISH_MONK, dex: 14, ac: 14 }],
+        ["include DEX when sluggish",                                        { ...ORCISH_MONK, dex: 6, ac: 10 }],
+        ["include WIS-1 when wise",                                          { ...ORCISH_MONK, wis: 14, ac: 13 }],
+        ["doesn't include WIS when foolish",                                 { ...ORCISH_MONK, wis: 6, ac: 12 }],
+        ["include DEX and WIS-1 when zippy and wise",                        { ...ORCISH_MONK, dex: 14, wis: 14, ac: 15 }],
+        ["include DEX only when zippy and foolish",                          { ...ORCISH_MONK, dex: 14, wis: 6, ac: 14 }],
+        ["include DEX and WIS-1 when sluggish and wise",                     { ...ORCISH_MONK, dex: 6, wis: 14, ac: 11 }],
+        ["include DEX only when sluggish and foolish",                       { ...ORCISH_MONK, dex: 6, wis: 6, ac: 10 }],
+        ["doesn't include DEX when zippy vs. a rogue",                       { ...ORCISH_MONK, attackerClass: 'Rogue', dex: 14, ac: 12 }],
+        ["includes DEX when sluggish vs. a rogue",                           { ...ORCISH_MONK, attackerClass: 'Rogue', dex: 6, ac: 10 }],
+        ["doesn't include WIS when wise vs. a rogue",                        { ...ORCISH_MONK, attackerClass: 'Rogue', wis: 14, ac: 12 }],
+        ["doesn't include WIS when foolish vs. a rogue",                     { ...ORCISH_MONK, attackerClass: 'Rogue', wis: 6, ac: 12 }],
+        ["doesn't include DEX nor WIS when zippy and wise vs. a rogue",      { ...ORCISH_MONK, attackerClass: 'Rogue', dex: 14, wis: 14, ac: 12 }],
+        ["doesn't include DEX when zippy and foolish vs. a rogue",           { ...ORCISH_MONK, attackerClass: 'Rogue', dex: 14, wis: 6, ac: 12 }],
+        ["doesn't include DEX and WIS-1 when sluggish and wise vs. a rogue", { ...ORCISH_MONK, attackerClass: 'Rogue', dex: 6, wis: 14, ac: 10 }],
+        ["includes DEX when sluggish and foolish vs. a rogue",               { ...ORCISH_MONK, attackerClass: 'Rogue', dex: 6, wis: 6, ac: 10 }],
+      ])("%s", (_, data) => validateArmorClass(data))
+    })
 
     function validateArmorClass(data) {
       let attacker = new Hero()
